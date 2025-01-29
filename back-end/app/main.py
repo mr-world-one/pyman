2aa82185b4accbe8bd6eecb8c6fd3795db257bb9
Markiyan Bevz <bevzm50@gmail.com>
2025-01-29 16:11:00 +0200

    Initialization of DB has been implemented in main.

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from routers import temporary_router
-
+from database import engine, Base
 
 app = FastAPI()
 
+# Кріейтимо таблиці при запуску програми
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+@app.on_event("startup")
+async def startup():
+    await init_db()
 
 app.include_router(temporary_router.router)
 
