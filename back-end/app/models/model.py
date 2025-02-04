b8bafa5f791883c7122993bf1e373e7795a67f2c
Markiyan Bevz <bevzm50@gmail.com>
2025-02-04 17:06:44 +0200

    model.py adjusted to implementation of authorization.

--- back-end/app/models/model.py
+++ back-end/app/models/model.py
@@ -5,6 +5,6 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    email = Column(String, unique=True, nullable=False)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
