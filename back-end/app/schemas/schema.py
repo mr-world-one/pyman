698580eb2921b04a0d05f7091592edbb424c0f21
Markiyan Bevz <bevzm50@gmail.com>
2025-02-04 17:00:08 +0200

    schemas.py was adjusted to create authorization

--- back-end/app/schemas/schema.py
+++ back-end/app/schemas/schema.py
@@ -1,15 +1,21 @@
 from pydantic import BaseModel, EmailStr
 
 class UserCreate(BaseModel):
-    name: str
+    Username: str
     email: EmailStr
+    password: str
 
-class UserResponse(UserCreate):
+class UserResponse(BaseModel):
     id: int
+    username:str
+    email: str
 
     class Config:
         from_attributes = True
 
 class Token(BaseModel):
-    access_token: str
-    token_type: str
+    access_token:str
+    token_type:str
+
+class TokenData(BaseModel):
+    username: str | None = None
