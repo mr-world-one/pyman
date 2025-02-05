a4591c19ae0439f7df61f355e8e70a529bba582a
Maxym <makko.22mv@gmail.com>
2025-02-05 21:41:11 +0200

    Added new front-project

---
+++ front-project/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about-us">
+    <p class="intro">
+      Вітаємо на головній сторінці! Треба знайти чи порівняти ціну - ви у правильному місці!
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  };
+</script>
+
+<style scoped>
+  .about-us {
+    text-align: center;
+    padding: 5rem;
+    max-width: 3000px;
+    margin: 0 auto; 
+  }
+
+  .intro {
+    font-size: 1.2rem;
+    margin-bottom: 1rem;
+  }
+</style>
