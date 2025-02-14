cf13fd95367631fb58cae7b144d4f44ee9ee4b85
Taras Mazepa <67551103+kolbaser228@users.noreply.github.com>
2025-02-14 10:26:06 +0200

    Delete some useless code lines

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -1,47 +1,9 @@
+#Third-party imports
 from fastapi import FastAPI
+
+#Local imports
 from app.routers import temporary_router, authentication, xpath
 from app.database import engine, Base
-
-
-
-import logging
-from fastapi import FastAPI, Request
-from starlette.middleware.base import BaseHTTPMiddleware
-
-app = FastAPI()
-
-# Увімкнути всі логи
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger("uvicorn.error")
-
-class LogExceptionMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
-        try:
-            response = await call_next(request)
-            return response
-        except Exception as e:
-            logger.error(f"HTTP 500: {str(e)}", exc_info=True)  #  Лог помилки
-            raise e  # Перепіднімаємо помилку, щоб FastAPI її коректно обробив
-
-# Додаємо middleware у FastAPI
-app.add_middleware(LogExceptionMiddleware)
-
-from fastapi.middleware.trustedhost import TrustedHostMiddleware
-
-app.add_middleware(TrustedHostMiddleware, allowed_hosts=["*"])
-app.debug = True  #  Включає детальне логування
-
-from fastapi.responses import JSONResponse
-from fastapi.exception_handlers import HTTPException
-from starlette.requests import Request
-
-@app.exception_handler(Exception)
-async def global_exception_handler(request: Request, exc: Exception):
-    logger.error(f" Внутрішня помилка сервера: {exc}", exc_info=True)
-    return JSONResponse(status_code=500, content={"detail": "Internal Server Error"})
-
-
-
 
 app = FastAPI()
 
